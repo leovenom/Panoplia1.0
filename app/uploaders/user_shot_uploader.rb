6bf2bcfa35f237dd8f29f3c29001608de823d5a0
leovenom <leonardt@oputlook.com>
2020-05-26 20:55:31 +0100

    navbar avatar and dropdown logout

--- app/uploaders/user_shot_uploader.rb
+++ app/uploaders/user_shot_uploader.rb
@@ -34,7 +34,7 @@ class UserShotUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [800, 600]
   end
   version :thumb do
-    process resize_to_fit: [400, 300]
+    process resize_to_fit: [00, 300]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
