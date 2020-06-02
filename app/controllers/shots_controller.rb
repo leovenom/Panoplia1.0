523103eaae20352c7b8fde6ae93f7a1bbe5b90f6
leovenom <leonardt@oputlook.com>
2020-06-02 22:51:45 +0100

    show page

--- app/controllers/shots_controller.rb
+++ app/controllers/shots_controller.rb
@@ -10,7 +10,7 @@ class ShotsController < ApplicationController
     @all_shots = Shot.includes(:user).where.not(id: @shot).order('shots.created_at DESC')
     #@shots = Shot.includes(:user).where.not(id: @shot).where(users: { entity: false}).order('shots.created_at DESC')
     @shots = @all_shots.select { |shot| !shot.user.entity? }
-    @random_shot = Shot.where.not(id: @shot).order("RANDOM()").first
+    #@random_shot = Shot.where.not(id: @shot).order("RANDOM()").first
     #@entity_shot = Shot.includes(:user).where.not(id: @shot).where(users: { entity: true}).order('shots.created_at DESC').first
     @entity_shots = @all_shots.select { |shot| shot.user.entity? }
 
@@ -20,7 +20,7 @@ class ShotsController < ApplicationController
   # GET /shots/1
   # GET /shots/1.json
   def show
-    @random_shot = Shot.where.not(id: @shot).order("RANDOM()").first
+    @random_shot = Shot.includes(:user).where.not(id: @shot).where(users: { entity: false}).order("RANDOM()").first
   end
 
   # GET /shots/new
