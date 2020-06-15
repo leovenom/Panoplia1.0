1d5fc324247a906e629363aa8f92b3dae9f34355
leovenom <leonardt@oputlook.com>
2020-06-15 21:55:37 +0100

    job board

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -2,7 +2,7 @@ module JobsHelper
 
   def job_type(job_type)
     if job_type == "Full-time"
-      content_tag :span, "#{job_type}", class: "tag is-primary"
+      content_tag :span, "#{job_type}", class: "tag is-primaryy"
     elsif job_type == "Part-time"
       content_tag :span, "#{job_type}", class: "tag is-link"
     elsif job_type == "Freelance"
