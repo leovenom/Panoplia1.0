bf4df1d712627d62ebece20e71cbfb2512ae813a
leovenom <leonardt@oputlook.com>
2020-06-16 20:41:48 +0100

    shearch params

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,8 +1,23 @@
 class SearchesController < ApplicationController
   def index
     @shots = Shot.where(["title LIKE ?", "%#{params[:search]}%"])
+    @shots_description = Shot.where(["description LIKE ?", "%#{params[:search]}%"])
+    @shots_user_shot = Shot.where(["user_shot LIKE ?", "%#{params[:search]}%"])
+    @shots = @shots_description + @shots_user_shot
     @jobs = Job.where(["title LIKE ?", "%#{params[:search]}%"])
-    @users  = User.where(["name LIKE ?", "%#{params[:id]}%"])
+
+    # if params[:search].present? && params[:search][:query].present?
+    #   @shots = policy_scope(Shot).where("title ILIKE '%#{params[:search][:query]}%'")
+    #   @shots_description = policy_scope(Shot).where("description ILIKE '%#{params[:search][:query]}%'")
+    #   @shots_user_shot = policy_scope(Shot).where("user_shot ILIKE '%#{params[:search][:query]}%'")
+    #   @shots = @shots_description + @shots_user_shot + @shots
+    #   else
+    #   @shots = policy_scope(Shot).order(created_at: :desc)
+    #   end
+  
+    #   if params[:search].present? && params[:search][:query].present?
+    #   @jobs = policy_scope(JOB).where(["title ILIKE ?", "%#{params[:search]}%"])
+    #   end
   end
 
   def new
@@ -29,7 +44,7 @@ class SearchesController < ApplicationController
   private
 
   def search_params
-    params.require(:search).permit(:keywords,:user_shot, :title, :description, :location, :art_type, :job_type, :name, :username, :bio, :art, :type, :entity)
+    params.require(:search).permit(:keywords, :user_shot, :title, :description, :location, :art_type, :job_type, :name, :username, :bio, :art, :type, :entity)
   end
 
 end
