48ad601cc6c6a996a1ba09554e317e381af3e7d6
leovenom <leonardt@oputlook.com>
2020-06-25 14:15:57 +0100

    ttf font

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module DribbbleClone
     config.load_defaults 5.1
 
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-    config.assets.precompile += %w(.svg .eot .woff .ttf .otf)
+    config.assets.precompile += %w(.svg .eot .woff .ttf)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
