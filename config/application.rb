5773eeec4d3adff19a3053981b2e83078017f1fa
leovenom <leonardt@oputlook.com>
2020-06-25 19:14:16 +0100

    commit_name

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module DribbbleClone
     config.load_defaults 5.1
 
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
