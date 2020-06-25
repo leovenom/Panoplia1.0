4def3dce9e91ea581e066388f0054e1f69985422
leovenom <leonardt@oputlook.com>
2020-06-25 11:51:09 +0100

    config

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ module DribbbleClone
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    config.assets.paths << Rails.root.join('app', 'assets', 'fonts', 'videos')
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.assets.enabled = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
