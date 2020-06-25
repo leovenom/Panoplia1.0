de87697ce59be3e1784d6e5ba978a1e7906d2338
leovenom <leonardt@oputlook.com>
2020-06-25 15:20:41 +0100

    @random_shot.user_shot_url

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,15 @@ module DribbbleClone
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
+
+  if Rails.env.production?
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+  end
+  
 end
 
