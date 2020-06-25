70286ea0f1f80deeadbedf2327e371a599cf1414
leovenom <leonardt@oputlook.com>
2020-06-25 10:23:33 +0100

    application

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module DribbbleClone
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   end
 end
 
-config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
