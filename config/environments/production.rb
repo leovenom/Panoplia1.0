efa4a564a6f822395638715212a3ca01a44fe5b5
leovenom <leonardt@oputlook.com>
2020-06-26 15:10:08 +0100

    fonts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,6 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # Code is not reloaded between requests.
   config.cache_classes = true
-
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
