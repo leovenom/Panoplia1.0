a67c752d68a93e430b5789499e12d59a0b23eace
leovenom <leonardt@oputlook.com>
2020-07-01 18:12:36 +0100

    okk

--- app/assets/javascripts/shots.js
+++ app/assets/javascripts/shots.js
@@ -209,3 +209,22 @@ document.addEventListener("turbolinks:load", function() {
             fon4.className = 'amatic';
           }
         }
+
+        var carousels = bulmaCarousel.attach('.carousel', options);
+
+        // Loop on each carousel initialized
+        for(var i = 0; i < carousels.length; i++) {
+          // Add listener to  event
+          carousels[i].on('before:show', state => {
+            console.log(state);
+          });
+        }
+        
+        // Access to bulmaCarousel instance of an element
+        var element = document.querySelector('#my-element');
+        if (element && element.bulmaCarousel) {
+          // bulmaCarousel instance is available as element.bulmaCarousel
+          element.bulmaCarousel.on('before-show', function(state) {
+            console.log(state);
+          });
+        }
