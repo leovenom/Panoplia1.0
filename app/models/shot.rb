3464d2ff1d6e4c8233d2e6f9b9c3c4d37822ca5f
leovenom <leonardt@oputlook.com>
2020-07-27 10:48:38 +0100

    max size

--- app/models/shot.rb
+++ app/models/shot.rb
@@ -5,4 +5,12 @@ class Shot < ApplicationRecord
 	mount_uploader :user_shot, UserShotUploader
 	is_impressionable
 	acts_as_votable
+
+	validate :user_shot_size_validation
+
+	private
+
+  def user_shot_size_validation
+    errors[:user_shot] << "should be less than 2MB" if user_shot.size > 2.megabytes
+  end
 end
