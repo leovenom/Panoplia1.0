911286861fc8ef821d97b4eb9181502c1929e970
leovenom <leonardt@oputlook.com>
2020-08-06 16:33:16 +0100

    mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,5 +57,4 @@ Rails.application.configure do
 
   # devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :smtp
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "dribbble_clone_#{Rails.env}"
   config.action_mailer.perform_caching = true
-  config.action_mailer.default_url_options = { host: 'https://panoplia.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => ENV['MAILER_URL'] }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
