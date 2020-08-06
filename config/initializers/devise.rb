a6a2b39009a236390f1cb95fca5f0836b8a6e572
leovenom <leonardt@oputlook.com>
2020-08-06 19:01:29 +0100

    email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'Leonardt-dev@example.com'
+  config.mailer_sender = 'panoplia@outlook.pt'
 
   # Configure the class responsible to send e-mails.
    config.mailer = 'Devise::Mailer'
