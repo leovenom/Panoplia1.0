617ae99827900c0682c345fdc8e531a2b5ed5d95
leovenom <leonardt@oputlook.com>
2020-12-07 15:20:11 +0100

    hamburguer navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,33 +32,69 @@ document.addEventListener('DOMContentLoaded', () => {
 
 //navbar hamburguer
 
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
 
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+//   // Get all "navbar-burger" elements
+//   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
+//   // Check if there are any navbar burgers
+//   if ($navbarBurgers.length > 0) {
 
-    // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
+//     // Add a click event on each of them
+//     $navbarBurgers.forEach( el => {
+//       el.addEventListener('click', () => {
 
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
+//         // Get the target from the "data-target" attribute
+//         const target = el.dataset.target;
+//         const $target = document.getElementById(target);
 
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
+//         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+//         el.classList.toggle('is-active');
+//         $target.classList.toggle('is-active');
 
-      });
-    });
-  }
+//       });
+//     });
+//   }
 
-});
+// });
 
+// $(document).ready(function() {
 
+//   // Check for click events on the navbar burger icon
+//   $(".navbar-burger").click(function() {
+
+//       // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+//       $(".navbar-burger").toggleClass("is-active");
+//       $(".navbar-menu").toggleClass("is-active");
+
+//   });
+// });
+
+// document.addEventListener('DOMContentLoaded', function () {
+
+//   // Get all "navbar-burger" elements
+//   var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+//   // Check if there are any navbar burgers
+//   if ($navbarBurgers.length > 0) {
+
+//     // Add a click event on each of them
+//     $navbarBurgers.forEach(function ($el) {
+//       $el.addEventListener('click', function () {
+
+//         // Get the target from the "data-target" attribute
+//         var target = $el.dataset.target;
+//         var $target = document.getElementById(target);
+
+//         // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+//         $el.classList.toggle('is-active');
+//         $target.classList.toggle('is-active');
+
+//       });
+//     });
+//   }
+
+// });
 
 document.querySelectorAll('.navbar-link').forEach(function(navbarLink){
   navbarLink.addEventListener('click', function(){
