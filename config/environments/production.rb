8e169c107cfd042f028a86a78bc2bef76f473f74
leovenom <leonardt@oputlook.com>
2021-01-26 00:06:05 +0100

    classic

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,8 +97,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    :user_name => ENV["apikey"],
-    :password => ENV['SENDGRID_API_KEY'],
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'panoplia.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
