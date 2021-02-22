80fbe6e2a1ccde1b54223179532b2f6d34eafe6f
leovenom <leonardt@oputlook.com>
2021-02-22 12:34:14 +0100

    added sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,17 +92,33 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
-  Rails.application.routes.default_url_options[:host] = 'panoplia.herokuapp.com'
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'panoplia.herokuapp.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+#   config.active_record.dump_schema_after_migration = false
+#   Rails.application.routes.default_url_options[:host] = 'panoplia.herokuapp.com'
+#   config.action_mailer.delivery_method = :smtp
+#   config.action_mailer.perform_deliveries = true
+#   config.action_mailer.smtp_settings = {
+#     :username => ENV['SENDGRID_USERNAME'],
+#     :password => ENV['SENDGRID_PASSWORD'],
+#     :domain => 'panoplia.herokuapp.com',
+#     :address => 'smtp.sendgrid.net',
+#     :port => 587,
+#     :authentication => :plain,
+#     :enable_starttls_auto => true
+#   }
+# end
+
+config.active_record.dump_schema_after_migration = false
+Rails.application.routes.default_url_options[:host] = 'panoplia.herokuapp.com'
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :api_key = ENV['SENDGRID_API_KEY'],
+  :domain => 'panoplia.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
 end
