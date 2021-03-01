d81b00b32fef0ba8f87eabe8c0eeb875076e4b99
Leonardt Lauenstein <leonardt@leonardts-air.localdomain>
2021-03-01 14:28:43 +0100

    removed sign up

--- db/migrate/20200806114034_add_confirmable_to_devise.rb
+++
@@ -1,7 +0,0 @@
-class AddConfirmableToDevise < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :confirmation_token, :string
-    add_column :users, :confirmed_at, :datetime
-    add_column :users, :confirmation_sent_at, :datetime
-  end
-end
